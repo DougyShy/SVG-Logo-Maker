b6b5f220053570e69ae2afdb100d043ebc8b6f3a
Doug Scheible <doug_scheible@yahoo.com>
2024-03-28 15:42:23 -0500

    test working now to index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const {Triangle, Circle, Square} = require('../shapes.js');
+
+const generateSVG = require('./lib/generateSVG.js');
+
+const text = 'CDS';
+const text_color = "blue";
+const shape_color = "red";
+const shape = new Circle("CDS", "red", "blue");
+
+
+
+  
